0b2eebdf600d79ba7806467131bb3a1a15ff0cbd
avī2 <mr.avi.py@gmail.com>
2023-04-09 19:54:36 +0530

    dDAD

---
+++ usingprint.py
@@ -0,0 +1,8 @@
+#PRINT FORMAT AND %s
+# code - print('{}{}{}'.format(0,1,2))
+# code - print('%s %s %s'%(0,1,2))
+a='python (str)'
+b=475735 
+c=[1,2,3,3]
+print('ada {} adad {} asfas {}'.format(a,b,c))
+print('%s fgfdh %s rddh %s rdhd'%(a,b,c))
